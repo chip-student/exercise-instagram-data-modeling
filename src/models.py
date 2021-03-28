e0fa437044fefa96b7977e2806e4230f0040f35b
chip-student <omoram28@gmail.com>
2021-03-28 23:57:04 +0000

    Cambios instagram model version 1.0

--- src/models.py
+++ src/models.py
@@ -5,26 +5,57 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
+from sqlalchemy import DateTime
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    iduser = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    password= Column(String(50), nullable=False)
+    email= Column(String(50), nullable=False)
+    created= Column(DateTime, nullable=False)
+    updated= Column(DateTime, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Post(Base):
+    __tablename__ = 'post'
+    idpost = Column(Integer, primary_key=True)
+    created = Column(DateTime, nullable=False)
+    updated = Column(DateTime, nullable=False)
+    total_like=Column(Integer, nullable=False)
+    total_comment=Column(Integer, nullable=False)
+    iduser = Column(Integer, ForeignKey('user.iduser'))
+    user = relationship(User)
+
+class Media(Base):
+    __tablename__ = 'media'
+    idmedia = Column(Integer, primary_key=True)
+    tipo= Column(String(100), nullable=False)
+    url= Column(String(300), nullable=False)
+    idpost = Column(Integer, ForeignKey('post.idpost'))
+    post = relationship(Post)
+
+class Post_Like(Base):
+    __tablename__ = 'post_like'
+    idpostlike= Column(Integer, primary_key=True)
+    iduser = Column(Integer, ForeignKey('user.iduser'))
+    user = relationship(User)
+    idpost = Column(Integer, ForeignKey('post.idpost'))
+    post = relationship(Post)
+    created= Column(DateTime, nullable=False)
+    updated= Column(DateTime, nullable=False)
+
+class Post_Comment(Base):
+    __tablename__ = 'post_comment'
+    idpostcomment= Column(Integer, primary_key=True)
+    iduser = Column(Integer, ForeignKey('user.iduser'))
+    user = relationship(User)
+    idpost = Column(Integer, ForeignKey('post.idpost'))
+    post = relationship(Post)
+    comment = Column(String(200), nullable=False)
+    created= Column(DateTime, nullable=False)
+    updated= Column(DateTime, nullable=False)
 
     def to_dict(self):
         return {}
